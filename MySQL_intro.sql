3366a637545a43ec5f0a8d4e5afa9131bfd2425d
Benniedollypy <bendyy001@gmail.com>
2026-02-07 06:56:15 +0100

    Arranged my sql syntax with indentations and comments

--- MySQL_intro.sql
+++ MySQL_intro.sql
@@ -1,67 +1,133 @@
-use mysql;
-show tables;
-select * from func;
-create database dolly_app;
-show databases
+-- created a new database called 'dolly_app'
+        create database dolly_app;
 
-create table emp_details (Name varchar (25), age int, sex char(1), 
-doj date, city varchar (15), salary float);
+        show databases
 
-describe emp_details;
+-- Using mysql database 
 
-insert into emp_details
-values("Jimmy", 35, "M", "2005-05-30", "Chicago", 70000),
-("Shane", 30, "M", "1996-06-25", "Seattle", 55000),
-("Mary", 28, "F", "2009-03-10", "Boston", 62000),
-("Dwayne", 37, "M", "2011-07-12", "Austin", 57000),
-("Sarah", 32, "F", "2017-10-17", "New York", 72000),
-("Ammy", 35, "F", "2014-12-20", "Seattle", 80000);
+        use mysql;
 
-Select * from emp_details;
+-- created table 'emp_details' with columns having variable declaration 
 
-select distinct city from emp_details;
 
-select count(name) as cunt_name from emp_details; 
+        create table emp_details (Name varchar (25), age int, sex char(1), 
+        doj date, city varchar (15), salary float);
 
-select sum(salary) as Total_salary from emp_details;
+        show tables;
 
-select Avg(salary) as Avg_salary from emp_details;
+-- PRACTICE ON THE USE OF AGGREGATE FUNCTIONS
 
-Select name, age, salary from emp_details;
+       select * from func;
 
-Select * from emp_details where age > 30;
+-- decription of table
 
-Select * from emp_details where sex = "F";
+        describe emp_details;
 
-Select * from emp_details where city = "Chicago" or city = "Austin";
+-- Updated data into the 'emp_details' table
 
-Select * from emp_details where city in('Chicago', 'Austin');
+        insert into emp_details
+        values("Jimmy", 35, "M", "2005-05-30", "Chicago", 70000),
+        ("Shane", 30, "M", "1996-06-25", "Seattle", 55000),
+        ("Mary", 28, "F", "2009-03-10", "Boston", 62000),
+        ("Dwayne", 37, "M", "2011-07-12", "Austin", 57000),
+        ("Sarah", 32, "F", "2017-10-17", "New York", 72000),
+        ("Ammy", 35, "F", "2014-12-20", "Seattle", 80000);
 
-Select * from emp_details where
-doj between '2000-01-01' and '2010-12-31';
+-- checking the updated data rows
 
-select * from emp_details where
-age > 30 and sex = 'm'
+        Select * from emp_details;
 
-select sex,  sum(salary) as total_salary from emp_details 
-group by sex
+--confirm the data in a distinct column 'city'
 
-select * from emp_details order by salary desc
+        select distinct city from emp_details;
 
-select curdate()
+-- exercise on count function
 
-select now()
+        select count(name) as cunt_name from emp_details; 
 
-#String functon
+-- exercise on sum function
 
-select concat('OKE Adedolapo', ' is', ' a', ' software engineer and a data analyst')
+        select sum(salary) as Total_salary from emp_details;
 
-#Replace function
+-- exercise on Avg function
 
-select replace('orange is a vegetable', 'vegetable', 'fruit') 
+        select Avg(salary) as Avg_salary from emp_details;
+
+-- Generating columns of interest from the 'emp_details' table
+
+        Select name, age, salary from emp_details;
+
+-- END of the FUNCTION practice
+/* Other functions includes the following
+Max func
+Min func
+*/
+
+/* USING THE "WHERE" clause in sql statements
+
+Works in SELECT, UPDATE, DELETE statements.
+Can use logical operators:
+= , > , < , >= , <= , <>
+AND, OR, NOT
+Filters rows before any aggregation (before GROUP BY or SUM() etc.)
+*/
+
+        Select name, age from emp_details where age > 30;
+
+        Select * from emp_details where sex = "F";
+
+--using the "WHERE" and the "OR" clause
+
+        Select * from emp_details where city = "Chicago" or city = "Austin";
+
+--using the "WHERE" and the "IN" clause
+
+        Select * from emp_details where city in('Chicago', 'Austin');
+
+--using the "WHERE", "BETWEEN" and the "AND" clause
+
+        Select * from emp_details where
+        doj between '2000-01-01' and '2010-12-31';
+
+--using 'logical operators' with "WHERE" and the "AND" clause
+
+        select * from emp_details where
+        age > 30 and sex = 'm'
+
+
+--Brief intro into "GROUP BY" statement
+        select sex,  sum(salary) as total_salary from emp_details 
+        group by sex
+
+--using the "ORDER" and "DESC" statement
+
+        select * from emp_details order by salary desc
+
+/*Exercises on buil-in sql functions 
+like CURDATE(), NOW(), DATEDIFF(), CONCAT()
+*/
+        select curdate()
+
+        select now()
+
+-- STRING FUNCTION
+
+        select concat('OKE Adedolapo', ' is', ' a', ' software engineer and a data analyst')
+
+-- Replace function
+
+        select replace('orange is a vegetable', 'vegetable', 'fruit') 
+
+-- Detect ASCII code
 
 select ascii('A')
-
+/*CLASSIFICATION OF SQL BUILT-IN FUNCTIONS
+Type	                            Examples	                                  Description
+Date & Time	             CURDATE(), NOW(), DATE_ADD()	                   Returns or manipulates date/time values
+String	                 CONCAT(), UPPER(), LOWER(), SUBSTRING()	       Manipulate or analyze text
+Numeric	                 ROUND(), ABS(), CEIL(), FLOOR()	               Perform calculations on numbers
+Aggregate	             SUM(), AVG(), COUNT(), MAX(), MIN()	           Summarize multiple rows
+*/
 
 
 
